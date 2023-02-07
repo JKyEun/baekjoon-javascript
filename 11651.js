f005ce03b7498b1497b1c4e707bb4dee90c8247e
JKyEun <j56237@naver.com>
2023-02-07 09:00:38 +0900

    백준 단계별 풀이 11651

---
+++ 11651.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
+const N = input.shift();
+let arr = [];
+
+for (let i = 0; i < N; i++) {
+  arr.push(input[i].split(" ").map(Number));
+}
+
+arr.sort(function(a, b) {
+  if (a[1] == b[1]) {
+    return a[0] - b[0];
+  } else {
+    return a[1] - b[1];
+  }
+});
+
+for (let i = 0; i < N; i++) {
+  arr[i] = arr[i].join(' ');
+}
+
+console.log(arr.join('\n'));
