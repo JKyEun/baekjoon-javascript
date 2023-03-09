1a2c57ac94d7e2cd0344016fc9ed75a530235ae3
JKyEun <j56237@naver.com>
2023-03-09 21:31:09 +0900

    기하1 - 1085

--- 10845.js
+++ 10845.js
@@ -1,12 +1,8 @@
 const fs = require('fs');
-const input = fs
-  .readFileSync('./input.txt')
-  .toString()
-  .trim()
-  .split('\n');
+const input = fs.readFileSync('./input.txt').toString().trim().split('\n');
 const N = +input.shift();
-let queue = [];
-let answer = [];
+const queue = [];
+const answer = [];
 
 for (let i = 0; i < N; i++) {
   if (input[i].includes('push')) {
---
+++ 1085.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const input = fs
+  .readFileSync('./dev/stdin')
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number);
+const [x, y, w, h] = input;
+
+const widthMin = Math.min(w - x, x);
+const heightMin = Math.min(h - y, y);
+
+console.log(Math.min(widthMin, heightMin));
