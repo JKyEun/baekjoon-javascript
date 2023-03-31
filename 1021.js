f99304d3920fdf79762f3d4d603f808bbe342c7b
JKyEun <j56237@naver.com>
2023-03-31 09:21:00 +0900

    큐, 덱 - 1021

---
+++ 1021.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const input = fs.readFileSync('./dev/stdin').toString().trim().split('\n');
+
+let [N, M] = input.shift().split(' ').map(Number);
+const idxArr = input[0].split(' ').map(Number);
+
+const queue = [];
+let answer = 0;
+
+for (let i = 0; i < N; i++) {
+  queue.push(i + 1);
+}
+
+const leftOperator = (arr) => {
+  arr.push(arr.shift());
+  answer++;
+};
+
+const rightOperator = (arr) => {
+  arr.unshift(arr.pop());
+  answer++;
+};
+
+const getDirection = (curN, idx) => {
+  if (curN / 2 >= idx) return 'left';
+  return 'right';
+};
+
+for (let i = 0; i < M; i++) {
+  if (getDirection(N, queue.indexOf(idxArr[i])) === 'left') {
+    while (queue[0] !== idxArr[i]) leftOperator(queue);
+    queue.shift();
+    N--;
+  } else {
+    while (queue[0] !== idxArr[i]) rightOperator(queue);
+    queue.shift();
+    N--;
+  }
+}
+
+console.log(answer);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,2 @@
 
-console.log(N, K);
+};
