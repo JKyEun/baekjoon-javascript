d8f5c33414260c7ec74ad8324645deb29f836207
JKyEun <j56237@naver.com>
2023-05-17 22:01:06 +0900

    9498

--- 9498.js
+++ 9498.js
@@ -2,17 +2,13 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin');
 
 if (input > 89) {
-    console.log("A");
-}
-else if (input > 79 && input < 90) {
-    console.log("B");
-}
-else if (input > 69 && input < 80) {
-    console.log("C");
-}
-else if (input > 59 && input < 70) {
-    console.log("D");
-}
-else {
-    console.log("F");
+  console.log('A');
+} else if (input > 79 && input < 90) {
+  console.log('B');
+} else if (input > 69 && input < 80) {
+  console.log('C');
+} else if (input > 59 && input < 70) {
+  console.log('D');
+} else {
+  console.log('F');
 }
